4875a9562408cf2d5b139086ca6a6b49a68aff9e
Andrey Nepomnyaschikh <And-dk@mail.ru>
2025-08-17 23:46:17 +0700

    commit

--- src/components/MobileFooterMenu.vue
+++ src/components/MobileFooterMenu.vue
@@ -5,54 +5,31 @@
                 <ul class="menu-nav__list">
                     <li class="menu-nav__item">
                         <a href="/" class="menu-nav__link">
-                            <svg class="menu-nav__icon" xmlns="http://www.w3.org/2000/svg" height="20"
-                                viewBox="-2 -2 24 24">
-                                <path fill="currentColor"
-                                    d="M18 18V7.132l-8-4.8l-8 4.8V18h4v-2.75a4 4 0 1 1 8 0V18h4zm-6 2v-4.75a2 2 0 1 0-4 0V20H2a2 2 0 0 1-2-2V7.132a2 2 0 0 1 .971-1.715l8-4.8a2 2 0 0 1 2.058 0l8 4.8A2 2 0 0 1 20 7.132V18a2 2 0 0 1-2 2h-6z" />
-                            </svg>
+                            <v-icon color="#afafaf" icon="mdi-home-outline" size="large"></v-icon>
                             <span class="menu-nav__title">Главная</span>
                         </a>
                     </li>
                     <li>
                         <a href="/" class="menu-nav__link">
-                            <svg class="menu-nav__icon" xmlns="http://www.w3.org/2000/svg" height="20"
-                                viewBox="0 0 24 24">
-                                <path fill="currentColor"
-                                    d="M11.5 9A2.5 2.5 0 0 1 14 6.5c1.1 0 2.03.71 2.37 1.69c.08.26.13.53.13.81a2.5 2.5 0 0 1-2.5 2.5c-1.09 0-2-.69-2.36-1.66c-.09-.26-.14-.55-.14-.84M5 9c0 4.5 5.08 10.66 6 11.81L10 22S3 14.25 3 9c0-3.17 2.11-5.85 5-6.71C6.16 3.94 5 6.33 5 9m9-7c3.86 0 7 3.13 7 7c0 5.25-7 13-7 13S7 14.25 7 9c0-3.87 3.14-7 7-7m0 2c-2.76 0-5 2.24-5 5c0 1 0 3 5 9.71C19 12 19 10 19 9c0-2.76-2.24-5-5-5Z" />
-                            </svg>
+                            <v-icon color="#afafaf" icon="mdi-map-marker-outline" size="large"></v-icon>
                             <span class="menu-nav__title">Магазины</span>
                         </a>
                     </li>
                     <li>
                         <a href="/" class="menu-nav__link">
-                            <svg class="menu-nav__icon" xmlns="http://www.w3.org/2000/svg" height="20"
-                                viewBox="0 0 512 512">
-                                <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10"
-                                    stroke-width="48" d="M88 152h336M88 256h336M88 360h336" />
-                            </svg>
+                            <v-icon color="#afafaf" icon="mdi-format-align-left" size="large"></v-icon>
                             <span class="menu-nav__title">Каталог</span>
                         </a>
                     </li>
                     <li>
                         <a href="/" class="menu-nav__link">
-                            <svg class="menu-nav__icon" xmlns="http://www.w3.org/2000/svg" height="20"
-                                viewBox="0 0 512 496">
-                                <path fill="currentColor"
-                                    d="M448 69H137l-3-12q-5-23-22.5-37.5T70 5H21q-9 0-15 6T0 27q0 9 6 15t15 6h49q19 0 22 17l49 256q6 21 23.5 34t38.5 13h202q10 0 16-6t6-15q0-22-22-22H203q-14 0-20-12l-2-9h214q20 0 36.5-12t22.5-31l58-123v-5q0-27-18.5-45.5T448 69zm-32 175v2q-3 15-21 15H173l-28-149h303q18 0 21 17zM256 432q0 18-12.5 30.5T213 475q-17 0-29.5-12.5T171 432t12.5-30.5T213 389q18 0 30.5 12.5T256 432zm171 0q0 18-12.5 30.5T384 475t-30.5-12.5T341 432t12.5-30.5T384 389t30.5 12.5T427 432z" />
-                            </svg>
+                            <v-icon color="#afafaf" icon="mdi-cart-outline" size="large"></v-icon>
                             <span class="menu-nav__title">Корзина</span>
                         </a>
                     </li>
                     <li>
                         <a href="/" class="menu-nav__link">
-                            <svg class="menu-nav__icon" xmlns="http://www.w3.org/2000/svg" height="20"
-                                viewBox="0 0 24 24">
-                                <g fill="none" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linejoin="round"
-                                        d="M4 18a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2Z" />
-                                    <circle cx="12" cy="7" r="3" />
-                                </g>
-                            </svg>
+                            <v-icon color="#afafaf" icon="mdi-account-outline" size="large"></v-icon>
                             <span class="menu-nav__title">Профиль</span>
                         </a>
                     </li>
@@ -99,7 +76,8 @@
     &__link {
         display: flex;
         flex-direction: column;
-        text-decoration:none;
+        text-decoration: none;
+        align-items: center
     }
 
     &__icon {
--- src/pages/Catalog.vue
+++ src/pages/Catalog.vue
@@ -2,31 +2,14 @@
     <div class="header">
         <div class="header-mobile">
             <div class="header-mobile__back">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 1024 1024">
-                    <path fill="#afafaf" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z" />
-                    <path fill="#afafaf"
-                        d="m237.248 512l265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z" />
-                </svg>
+                <v-icon color="#afafaf" icon="mdi-arrow-left" size="large"></v-icon>
             </div>
             <div class="header-mobile__search">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <path fill="#afafaf"
-                        d="M10 4a6 6 0 1 0 0 12a6 6 0 0 0 0-12zm-8 6a8 8 0 1 1 14.32 4.906l5.387 5.387a1 1 0 0 1-1.414 1.414l-5.387-5.387A8 8 0 0 1 2 10z" />
-                </svg>
+                <v-icon color="#afafaf" icon="mdi-magnify" size="large"></v-icon>
                 <input class="header-mobile__input" type="text" placeholder="Поиск по сайту">
-                <svg class="header-mobile__scan" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                    viewBox="0 0 24 24">
-                    <path fill="#afafaf" stroke="#afafaf" stroke-linecap="round" stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M3 7V5a2 2 0 0 1 2-2h2m10 0h2a2 2 0 0 1 2 2v2m0 10v2a2 2 0 0 1-2 2h-2M7 21H5a2 2 0 0 1-2-2v-2M8 7v10m4-10v10m5-10v10" />
-                </svg>
             </div>
             <div class="header-mobile__notification">
-                <svg class="header-mobile__notification-button" xmlns="http://www.w3.org/2000/svg" width="20"
-                    height="20" viewBox="0 0 24 24">
-                    <path fill="#afafaf"
-                        d="M4 8a8 8 0 1 1 16 0v4.697l2 3V20h-5.611a4.502 4.502 0 0 1-8.777 0H2v-4.303l2-3V8Zm5.708 12a2.5 2.5 0 0 0 4.584 0H9.708ZM12 2a6 6 0 0 0-6 6v5.303l-2 3V18h16v-1.697l-2-3V8a6 6 0 0 0-6-6Z" />
-                </svg>
+                <v-icon color="#afafaf" icon="mdi-bell-outline" size="large"></v-icon>
             </div>
 
         </div>
@@ -77,10 +60,6 @@
     &__input {
         width: 100%;
         padding: 0px 8px 0px 8px;
-    }
-
-    &__scan {
-        padding: 0px 0px 0px 0px;
     }
 
     &__notification {
